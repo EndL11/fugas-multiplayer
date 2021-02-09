e10f08a55a48a44a46d4b597ba2e7eed51b1b09f
EndL11 <podobailo.andriy@gmail.com>
2021-02-09 20:44:19 +0200

    added showInfo method, added console log to destructor

--- fugas-multiplayer/Player.hpp
+++ fugas-multiplayer/Player.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 class Player{
 	int m_id;
@@ -7,12 +8,17 @@ class Player{
 public:
 	Player() {};
 	Player(int t_id, std::string t_name, int t_rank): m_id(t_id), m_name(t_name), m_rank(t_rank) {};
-	~Player() {};
+	~Player() {
+		std::cout << "Player with id: " << this->m_id << " and name: " << m_name << " deleted" << std::endl;
+	};
 	void plusRank(int t_rank) {
 		this->m_rank += t_rank;
 	}
 	void minusRank(int t_rank) {
 		this->m_rank -= t_rank;
+	}
+	void showInfo() {
+		std::cout << "Player info: " << "\nID: " << m_id << "\tName: " << m_name << "\tRank: " << m_rank << "\n\n";
 	}
 	std::string name() {
 		return this->m_name;
