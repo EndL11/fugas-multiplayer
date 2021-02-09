0a2d07d2272dda356b435e01f31310ccc51a57b8
EndL11 <podobailo.andriy@gmail.com>
2021-02-09 12:50:44 +0200

    created class Player

---
+++ fugas-multiplayer/Player.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+class Player{
+	int m_id;
+	int m_rank;
+	std::string m_name;
+public:
+	Player() {};
+	Player(int t_id, std::string t_name, int t_rank): m_id(t_id), m_name(t_name), m_rank(t_rank) {};
+	~Player() {};
+	void plusRank(int t_rank) {
+		this->m_rank += t_rank;
+	}
+	void minusRank(int t_rank) {
+		this->m_rank -= t_rank;
+	}
+	std::string name() {
+		return this->m_name;
+	}
+	int id() {
+		return this->id;
+	}
+	int rank() {
+		return this->rank;
+	}
+};
