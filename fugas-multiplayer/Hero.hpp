4be9843cbae86f05e604736c018a8c036797ceb5
EndL11 <podobailo.andriy@gmail.com>
2021-02-09 20:34:09 +0200

    added destructor console log, and public getters, overridded compare operator and added showinfo method

--- fugas-multiplayer/Hero.hpp
+++ fugas-multiplayer/Hero.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <string>
 class Hero {
 	std::string m_name;
@@ -10,11 +11,25 @@ public:
 	Hero(int t_id, std::string t_name, int t_hp, int t_damage)
 		: m_id(t_id), m_name(t_name), m_hp(t_hp), m_damage(t_damage)
 	{};
-	~Hero() {};
+	~Hero() {
+		std::cout << "Hero with id: " << m_id << " and name: " << m_name << " deleted" << std::endl;
+	};
 	int hp() {
-		return this->hp;
+		return this->m_hp;
 	}
 	int damage() {
-		return this->damage;
+		return this->m_damage;
+	}
+	int id() {
+		return this->m_id;
+	}
+	std::string name() {
+		return this->m_name;
+	}
+	void showInfo() {
+		std::cout << "Hero info: " << "\nID: " << m_id << "\tName: " << m_name << "\tHp: " << m_hp << "\tDamage: " << m_damage << "\n\n";
+	}
+	friend bool operator==(const Hero &lhero, const Hero &rhero) {
+		return lhero.m_id == rhero.m_id;
 	}
 };
