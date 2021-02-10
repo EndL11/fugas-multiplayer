c55cda674287020838a5b20641eafe18648e3d83
Andriy Podobailo <podobailo.andriy@gmail.com>
2021-02-10 13:39:35 +0200

    deleted logs, created lists for destroying

--- fugas-multiplayer/GameManager.hpp
+++ fugas-multiplayer/GameManager.hpp
@@ -13,13 +13,13 @@ public:
 	~GameManager() {};
 
 	void PerformGameSession(std::list<Player> &t_players, std::list<Hero> &t_heroes) {
-		std::cout << "Players Count: " << t_players.size() << std::endl;
-		std::cout << "Heroes Count: " << t_heroes.size() << std::endl;
 		int rate = 25;
 		TeamManager team_manager;
+		std::list<Player> players = t_players;
+		std::list<Hero> heroes = t_heroes;
 		//	generating teams
-		Team team_one = team_manager.GenerateNewTeam(t_players, t_heroes);
-		Team team_two = team_manager.GenerateNewTeam(t_players, t_heroes);
+		Team team_one = team_manager.GenerateNewTeam(players, heroes);
+		Team team_two = team_manager.GenerateNewTeam(players, heroes);
 		//	showing each team info
 		team_manager.GetTeamInfo(team_one);
 		team_manager.GetTeamInfo(team_two);
